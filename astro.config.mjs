02f01b1fc32c581fb33f62639cf2121ea46f0870
Oscar Sharpe <oscarsharpe@proton.me>
2023-09-26 17:57:48 +0100

    Migrate from Astro V2 to V3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
+//import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
 import compress from 'astro-compress';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 
@@ -36,11 +37,26 @@ export default defineConfig({
       },
     }),
     sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
+  //  image({
+   //   serviceEntryPoint: '@astrojs/image/sharp',
+   // }),
     mdx(),
-
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
     ...whenExternalScripts(() =>
       partytown({
         config: { forward: ['dataLayer.push'] },
