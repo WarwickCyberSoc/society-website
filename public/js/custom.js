eeaaa7959da13fba3e5e1746ff83e47b8b836da0
Tom <tom@tomsteer.me>
2024-02-17 20:04:12 +0000

    Hamburger

---
+++ public/js/custom.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+	var x = document.getElementById("hamburgerPopout");
+	var y = document.getElementById("hamburgerContainer");
+	if (x.style.display === "flex") {
+		x.style.display = "none";
+		y.dataset.open = "false";
+	} else {
+		x.style.display = "flex";
+		y.dataset.open = "true";
+	}
+}
