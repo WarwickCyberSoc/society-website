cccb01e5a37bd7188232d868ea664d5bf2abc479
peterw18 <peterjw2005@gmail.com>
2025-04-26 22:25:16 +0100

    maybe works for timetable? pt.52 DEBUG

--- main.go
+++ main.go
@@ -179,6 +179,6 @@ func prepareSchedule(schedule *conferenceSchedule) map[string]bool {
             schedule.Events[i].RowSpan = 1
         }
     }
-
+	skipMap["poo"] = true
     return skipMap
 }
