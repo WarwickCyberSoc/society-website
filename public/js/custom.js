2583ee03fd8b1397722c6f64ba038d3f6ff68ec2
peterw18 <peterjw2005@gmail.com>
2025-05-06 12:18:10 +0100

    update JS ID

--- public/js/custom.js
+++ public/js/custom.js
@@ -29,7 +29,7 @@ function createCountDown(){
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-	document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+	document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
 	+ minutes + "m " + seconds + "s ";
 
 	if (distance < 0) {
